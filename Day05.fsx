2033b8285253117d70d1424cc7f230a0702e3470
Anton Herzog <info@antonherzog.com>
2018-10-08 00:21:37 +0200

    Cleanup logic.

--- Day05.fsx
+++ Day05.fsx
@@ -8,11 +8,13 @@ let md5 (data : byte array) : string =
     ||> Array.fold (fun sb b -> sb.Append(b.ToString("x2")))
     |> string
 
-let input = "wtnhxymk"
+let personalInput = "wtnhxymk"
 
-let hashWithSalt (salt:int) = (input + salt.ToString()) 
+let hashWithSalt input (salt:int) = (input + salt.ToString()) 
                                  |> System.Text.Encoding.ASCII.GetBytes 
                                  |> md5
+
+let personalHash = hashWithSalt <| personalInput
 
 let isInterestingHash (hash:string) = hash.StartsWith("00000")
 
@@ -20,27 +22,18 @@ let saveSixthChar hash state = Seq.item 5 hash :: state
 
 let stateIsFinal state = Seq.length state > 7
 
-let rec findPassword i (state:char list) =
-    let hash = hashWithSalt i
-    match isInterestingHash hash with
-    | true ->
-        let newChars = saveSixthChar hash state
-        match stateIsFinal newChars with
-        | true  -> List.rev newChars
-        | false -> findPassword (i+1) newChars
-    | false -> findPassword (i+1) state
+let solver isMatch saveToState isFinished i state =
+    let rec solve i state = 
+        let hash = personalHash i
+        match isMatch hash with
+        | true ->
+            let newChars = saveToState hash state
+            match isFinished newChars with
+            | true  -> List.rev newChars
+            | false -> solve (i+1) newChars
+        | false -> solve (i+1) state
+    solve i state
 
-
-let rec findPassword2 isMatch saveToState isFinished i state =
-    let hash = hashWithSalt i
-    match isMatch hash with
-    | true ->
-        let newChars = saveToState hash state
-        match isFinished newChars with
-        | true  -> List.rev newChars
-        | false -> findPassword (i+1) newChars
-    | false -> findPassword (i+1) state
-
-let solverPartOne = findPassword2 isInterestingHash saveSixthChar stateIsFinal 
+let solverPartOne = solver isInterestingHash saveSixthChar stateIsFinal 
 
 let resultPartOne = solverPartOne 0 List.Empty |> String.Concat
